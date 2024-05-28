05383ba0a83ad13923e576921c756e0ff572579a
Manuel Duran <manuelduran@Manuels-MacBook-Pro.local>
2024-05-28 12:03:20 -0700

    migrating

--- src/services/knex.ts
+++ src/services/knex.ts
@@ -5,4 +5,14 @@ import knexConfig from "../../knexfile.js";
 // Initialize Knex with the development configuration
 const knexClient = Knex(knexConfig.development);
 
+export const runMigrations = async () => {
+  try {
+    console.log("Running migrations...");
+    await knexClient.migrate.latest();
+    console.log("Migrations completed!");
+  } catch (error) {
+    console.error("Error running migrations:", error);
+  }
+};
+
 export default knexClient;
