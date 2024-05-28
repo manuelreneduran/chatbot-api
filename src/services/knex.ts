a4aedfda86ac548e8507bf18378aee6d5ef1a731
Manuel Duran <manuelduran@Manuels-MacBook-Pro.local>
2024-05-28 10:57:20 -0700

    renamed knex to knexclient

--- src/services/knex.ts
+++ src/services/knex.ts
@@ -3,6 +3,6 @@ import Knex from "knex";
 import knexConfig from "../../knexfile.js";
 
 // Initialize Knex with the development configuration
-const knex = Knex(knexConfig.development);
+const knexClient = Knex(knexConfig.development);
 
-export default knex;
+export default knexClient;
